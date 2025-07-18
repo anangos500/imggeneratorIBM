52cf8b68d38f5c96f574826312995fdea3b00eb3
anangos500 <anangos500@gmail.com>
2025-07-18 23:11:35 +0700

    Added foto-generator - Salin.js

---
+++ js/foto-generator - Salin.js
@@ -0,0 +1,8 @@
+function downloadFile(url, filename) {
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
